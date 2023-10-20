4ae6a5606871687050f124ac250cfbc9b3795fb1
Bianca Novaes <bnovaesolavo@gmail.com>
2023-10-20 10:57:47 -0300

    Consumer atualizado
    Signed-off-by: Bianca Novaes <bnovaesolavo@gmail.com>

--- src/consumer/ConsumerExample.java
+++ src/consumer/ConsumerExample.java
@@ -10,7 +10,7 @@ public class ConsumerExample {
     public static void main(String[] args) {
         List<Integer> numerosList = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 200);
 
-        //Criando o Consumer manualmente
+
         Consumer<Integer> imprimirPares = numero -> {
             if(numero % 2 == 0) {
                 System.out.println(numero);
